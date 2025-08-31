f37c3fcd6a7812178ed022705b7874da3b616d1e
warfather0991 <nakib1990saleh@gmail.com>
2025-09-01 00:13:50 +0600

    fix

--- src/main/java/com/eventlagbe/backend/Config/CorsConfig.java
+++ src/main/java/com/eventlagbe/backend/Config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                        "https://event-lagbe.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
